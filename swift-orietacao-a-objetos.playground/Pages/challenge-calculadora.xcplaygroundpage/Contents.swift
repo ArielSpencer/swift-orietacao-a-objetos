a8b7983c5281320ae0a72b196f0e0be8dfdbe398
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-13 23:03:04 -0300

    feat: add class calculator

--- swift-orietacao-a-objetos.playground/Pages/challenge-calculadora.xcplaygroundpage/Contents.swift
+++ swift-orietacao-a-objetos.playground/Pages/challenge-calculadora.xcplaygroundpage/Contents.swift
@@ -26,3 +26,37 @@ print("Soma = \(calculadoraStruct.soma())")
 print("Subtração = \(calculadoraStruct.subtracao())")
 print("Multiplicação = \(calculadoraStruct.multiplicacao())")
 print("Divisão = \(calculadoraStruct.divisao())")
+
+class CalculadoraClass {
+    var valor1: Double
+    var valor2: Double
+    
+    func soma() -> Double {
+        return valor1 + valor2
+    }
+    
+    func subtracao() -> Double {
+        return valor1 - valor2
+    }
+    
+    func multiplicacao() -> Double {
+        return valor1 * valor2
+    }
+    
+    func divisao() -> Double {
+        return valor1 / valor2
+    }
+    
+    init(valor1: Double, valor2: Double) {
+        self.valor1 = valor1
+        self.valor2 = valor2
+    }
+}
+
+var calculadoraClass = CalculadoraClass(valor1: 30, valor2: 15)
+
+print("Calculadora Class:")
+print("Soma = \(calculadoraClass.soma())")
+print("Subtração = \(calculadoraClass.subtracao())")
+print("Multiplicação = \(calculadoraClass.multiplicacao())")
+print("Divisão = \(calculadoraClass.divisao())")
