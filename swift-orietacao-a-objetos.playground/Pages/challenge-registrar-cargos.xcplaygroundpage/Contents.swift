ad5bc7406b749ace8f6afddb16c28299c9b962aa
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-15 23:00:31 -0300

    feat: add  seller class

--- swift-orietacao-a-objetos.playground/Pages/challenge-registrar-cargos.xcplaygroundpage/Contents.swift
+++ swift-orietacao-a-objetos.playground/Pages/challenge-registrar-cargos.xcplaygroundpage/Contents.swift
@@ -19,3 +19,19 @@ class Gerente: Empregado {
 
 let gerente = Gerente(nome: "Ariel", salario: 7500.0, departamento: "TI")
 print("Gerente: \(gerente.nome), Salário: \(gerente.salario), Departamento: \(gerente.departamento)")
+
+class Vendedor: Empregado {
+    var departamento: String
+    
+    init(nome: String, salario: Double, departamento: String) {
+        self.departamento = departamento
+        super.init(nome: nome, salario: salario)
+    }
+    
+    func percentualComissao(_ totalVendas: Int) -> Double {
+        return (0.1 * 50 * Double(totalVendas)) + self.salario
+    }
+}
+
+let vendedor = Vendedor(nome: "Maria", salario: 5000.0, departamento: "Vendas")
+print("Vendedor \(vendedor.nome): Salário: \(vendedor.salario), Comissão: \(vendedor.percentualComissao(100))")
