9623a3c26164b3b8ff3fb727dd6710c12744f65b
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-18 20:51:05 -0300

    feat: add enum

---
+++ swift-orietacao-a-objetos.playground/Pages/enumeracao.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+enum FormaDePagamento {
+    case pix
+    case boleto
+    case saldoEmConta
+}
+
+class Conta {
+    var saldo = 0.0
+    var nome: String
+    
+    func sacar(_ valor: Double) {
+        saldo -= valor
+    }
+    
+    func depositar(_ valor: Double) {
+        saldo += valor
+    }
+    
+    func pagamentoCartao(_ tipoPagamento: FormaDePagamento) {
+        switch tipoPagamento {
+        case .pix: print("O pagamento será realizado com pix.")
+        case .boleto: print("O pagamento será realizado com boleto.")
+        case .saldoEmConta: print("O pagamento será realizado com saldo em conta.")
+        }
+    }
+    
+    init(nome: String) {
+        self.nome = nome
+    }
+}
+
+var contaAriel: Conta = .init(nome: "Ariel")
+contaAriel.pagamentoCartao(.pix)
