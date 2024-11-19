dca2d04008123629d25cb2e62d1a61b7e8d08b6e
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-18 21:05:02 -0300

    feat: add raw values

--- swift-orietacao-a-objetos.playground/Pages/enumeracao.xcplaygroundpage/Contents.swift
+++ swift-orietacao-a-objetos.playground/Pages/enumeracao.xcplaygroundpage/Contents.swift
@@ -32,9 +32,30 @@ class Conta {
 var contaAriel: Conta = .init(nome: "Ariel")
 contaAriel.pagamentoCartao(.pix)
 
-enum Mes {
-    case janeiro, fevereiro, marco, abril, maio, junho, julho, agosto, setembro, outubro, novembro, dezembro
+//enum Mes {
+//    case janeiro, fevereiro, marco, abril, maio, junho, julho, agosto, setembro, outubro, novembro, dezembro
+//}
+//
+//var novembro = Mes.novembro
+//var fevereiro: Mes = .fevereiro
+
+// raw values:
+enum Mes: String {
+    case janeiro, fevereiro = "Fev", marco, abril, maio, junho, julho, agosto, setembro, outubro, novembro, dezembro
 }
 
 var novembro = Mes.novembro
+print(novembro.rawValue)
 var fevereiro: Mes = .fevereiro
+print(fevereiro.rawValue)
+
+enum Moeda: Double {
+    case umCentavo = 0.01
+    case cincoCentavos = 0.05
+    case dezCentavos = 0.10
+    case vinteCincoCentavos = 0.25
+    case cinquentaCentavos = 0.50
+}
+
+var moedaCincoCentavos = Moeda.cincoCentavos
+print(moedaCincoCentavos.rawValue)
